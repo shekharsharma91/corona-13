9f3c571b04a17d5292e31f786356722b2dea1333
shekharsharma91 <shekharsharma041@gmail.com>
2013-03-25 23:11:22 +0530

    complete

---
+++ 4/js/custom.js
@@ -0,0 +1,64 @@
+			
+		/* Creation takes times, copying doesn't. 
+Contact me i.e. Shekhar sharma( http://facebook.com/shekharsharma91) it you are having any problem in copying !!		*/	
+			
+			function toggleOverlay(){
+						var overlay = document.getElementById('overlay');
+						var specialBox = document.getElementById('specialBox');
+						overlay.style.opacity = .8;
+						if(overlay.style.display == "block"){
+							overlay.style.display = "none";
+							specialBox.style.display = "none";
+						} else {
+							overlay.style.display = "block";
+							specialBox.style.display = "block";
+						}
+					}
+	function toggleOverlay1(){
+						var overlay = document.getElementById('overlay');
+						var specialBox = document.getElementById('specialBoxG');
+						overlay.style.opacity = .7;
+						if(overlay.style.display == "block"){
+							overlay.style.display = "none";
+							specialBox.style.display = "none";
+						} else {
+							overlay.style.display = "block";
+							specialBox.style.display = "block";
+						}
+					}
+function toggleOverlay2(){
+						var overlay = document.getElementById('overlay');
+						var specialBox = document.getElementById('specialBoxC');
+						overlay.style.opacity = .7;
+						if(overlay.style.display == "block"){
+							overlay.style.display = "none";
+							specialBox.style.display = "none";
+						} else {
+							overlay.style.display = "block";
+							specialBox.style.display = "block";
+						}
+					}
+					function toggleOverlay3(){
+						var overlay = document.getElementById('overlay');
+						var specialBox = document.getElementById('specialBoxS');
+						overlay.style.opacity = .7;
+						if(overlay.style.display == "block"){
+							overlay.style.display = "none";
+							specialBox.style.display = "none";
+						} else {
+							overlay.style.display = "block";
+							specialBox.style.display = "block";
+						}
+					}
+					function toggleOverlay4(){
+						var overlay = document.getElementById('overlay');
+						var specialBox = document.getElementById('specialBoxR');
+						overlay.style.opacity = .7;
+						if(overlay.style.display == "block"){
+							overlay.style.display = "none";
+							specialBox.style.display = "none";
+						} else {
+							overlay.style.display = "block";
+							specialBox.style.display = "block";
+						}
+					}
---
+++ corona_13/index.php
@@ -0,0 +1,4 @@
+<?php
+
+header('Location: http://www.corona13.com/4/index1.html');
+?>
